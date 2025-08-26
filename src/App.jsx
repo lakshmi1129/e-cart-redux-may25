ed2a4f0ab9913e7183fa9d73ccb78c7602b3a295
lakshmi2911 <lakshmipriya1129@gmail.com>
2025-08-26 12:53:42 +0530

    Wishlist complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Wishlist from './pages/Wishlist'
+import Cart from './pages/Cart'
+import View from './pages/View'
+import Pnf from './pages/Pnf'
+import Footer from './components/Footer'
 
 
 function App() {
-
   return (
     <>
-   <h1 className="text-6xl text-amber-600">Ecart</h1>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/wishlist" element={<Wishlist />}></Route>
+        <Route path="/cart" element={<Cart />}></Route>
+        <Route path="/:id/view" element={<View />}></Route>
+        <Route path="/*" element={<Pnf />}></Route>
+      </Routes>
+      <Footer/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
